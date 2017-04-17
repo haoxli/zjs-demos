6f896db61cbea235f718bdca9aa6f441f49a7e18
cuiyanx <yanx.cui@intel.com>
2017-04-17 15:46:47 +0800

    [module] Add angle and speed chenge
    Signed-off-by: cuiyanx <yanx.cui@intel.com>

--- minicar/tests/test-selfcheck.js
+++ minicar/tests/test-selfcheck.js
@@ -23,55 +23,80 @@ var checkNum = 0;
 var checkTimer = setInterval(function() {
     checkNum++;
 
-    if (checkNum === 1 || checkNum === 5) {
+    if (checkNum === 3 || checkNum === 4) {
         steerer.left(45);
 
         console.log("Minicar steerer state - " + steerer.getSteererState());
     }
 
-    if (checkNum === 2 || checkNum === 6 ||
-        checkNum === 4 || checkNum === 8) {
+    if (checkNum === 1 || checkNum === 2 ||
+        checkNum === 5 ||checkNum === 6) {
+        steerer.left(30);
+
+        console.log("Minicar steerer state - " + steerer.getSteererState());
+    }
+
+    if (checkNum === 7 || checkNum === 14) {
         steerer.front();
 
         console.log("Minicar steerer state - " + steerer.getSteererState());
     }
 
-    if (checkNum === 3 || checkNum === 7) {
+    if (checkNum === 8 || checkNum === 9 ||
+        checkNum === 12 ||checkNum === 13) {
+        steerer.right(30);
+
+        console.log("Minicar steerer state - " + steerer.getSteererState());
+    }
+
+    if (checkNum === 10 || checkNum === 11) {
         steerer.right(45);
 
         console.log("Minicar steerer state - " + steerer.getSteererState());
     }
 
-    if (checkNum === 9) {
+    if (checkNum === 15) {
         console.log("Minicar selfcheck - steerer complete");
         console.log("Minicar selfcheck - driver start");
     }
 
-    if (checkNum === 10 || checkNum === 14) {
+    if (checkNum === 18 || checkNum === 19) {
         driver.forward(70);
 
         console.log("Minicar driver state - " + driver.getDriverState());
     }
 
-    if (checkNum === 11 || checkNum === 13) {
+    if (checkNum === 16 || checkNum === 17 || checkNum === 20 ||checkNum === 21) {
+        driver.forward(10);
+
+        console.log("Minicar driver state - " + driver.getDriverState());
+    }
+
+    if (checkNum === 22) {
         driver.coast();
 
         console.log("Minicar driver state - " + driver.getDriverState());
     }
 
-    if (checkNum === 15 || checkNum === 17) {
+    if (checkNum === 29) {
         driver.brake();
 
         console.log("Minicar driver state - " + driver.getDriverState());
     }
 
-    if (checkNum === 12 || checkNum === 16) {
+    if (checkNum === 25 || checkNum === 26) {
         driver.reverse(70);
 
         console.log("Minicar driver state - " + driver.getDriverState());
     }
 
-    if (checkNum === 18) {
+    if (checkNum === 23 || checkNum === 24 || checkNum === 27 ||checkNum === 28) {
+        driver.reverse(10);
+
+        console.log("Minicar driver state - " + driver.getDriverState());
+    }
+
+    if (checkNum === 30) {
         console.log("Minicar selfcheck - driver complete");
         console.log("Minicar selfcheck - complete");
         clearInterval(checkTimer);
